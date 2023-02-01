69cb6cb6d731d6dff3a97692b1dfb766080a8045
Nazar <nazar.lyubchinskyi@gmail.com>
2023-02-01 15:54:22 +0200

    Guess a number

--- hello.js
+++ hello.js
@@ -1,6 +1,12 @@
 alert('Задумайте число!');
 alert('Умножьте задуманное число на 2!');
 alert('Прибавьте к полученному результату 7!');
-const number = prompt('Введите что получилось');
-alert((number - 7) / 2);
-console.log(document);
+
+const userNumber = Number(prompt('Введите что получилось'));
+
+if (userNumber) {
+	alert((userNumber - 7) / 2);
+	console.log(typeof userNumber)
+} else {
+	alert('введіть цифри!!')
+}
